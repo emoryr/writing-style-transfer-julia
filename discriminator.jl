c83bb324af8f5c6fa29a2147529e1bd29342e113
Emory Freitas <ervf@cesar.org.br>
2022-02-02 11:16:43 -0400

    Implement forward discriminator
    Signed-off-by: Emory Freitas <emory.rvf@gmail.com>

--- discriminator.jl
+++ discriminator.jl
@@ -1,5 +1,6 @@
 using Flux
 using Functors
+using Images
 
 struct Discriminator
     embedding
@@ -13,11 +14,18 @@ function Discriminator(
     pad_idx::Int
 
 ) return Discriminator(
-    Embedding(vocab_size, embedding_dim),
+    Chain(Embedding(vocab_size, embedding_dim),
+        Pad((pad_idx, pad_idx))),
     Dense(embedding_dim, 1)
 )
 end
 
 function(net::Discriminator)(x)
-    return
+    embedded = net.embedding(x)
+
+    embedded = permutedims(embedded,(2, 1, 3))
+
+    pooled = squeeze(meanpool(embedded, (size(embedded)[2], 1)), 2)
+
+    return net.fc(pooled)
 end
